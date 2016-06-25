ae503fd267b611096ce10b5ced9d330221e102d7
Alex Williams <alex.h.willia@gmail.com>
2016-06-25 15:01:19 -0400

    remove lbase, dev sgd, add basic tests

--- src/StochasticOptimization.jl
+++ src/StochasticOptimization.jl
@@ -1,7 +1,19 @@
 module StochasticOptimization
 
-using LearnBase
+# using LearnBase
+
+# TEMPORARY - includes abstract types that will be imported from LearnBase
+abstract ParameterUpdaterState
+abstract ParameterUpdater
+abstract ParameterLoss
+macro _dimcheck(condition)
+    :(($(esc(condition))) || throw(DimensionMismatch("Dimensions of the parameters don't match")))
+end
 
 include("paramupdater.jl")
+
+export SGDUpdater,
+       SGDState,
+       param_update!
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,19 @@
 using StochasticOptimization
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+# ----- SGD with momentum ----- #
+dims = (2,3,4)
+state = SGDState(dims)
+@test size(state.v) == dims
+@test eltype(state.v) == Float64
+@test all(isapprox(state.v,zeros(dims)))
+
+x = randn(10)
+state = SGDState(x)
+@test all(isapprox(state.v,x))
+@test eltype(state.v) == eltype(x)
+
+state = SGDState(size(x))
+@test all(isapprox(state.v,zeros(size(x))))
+param_update!(state, SGDUpdater(;η=1.0), x)
+@test all(isapprox(state.v,-x))
